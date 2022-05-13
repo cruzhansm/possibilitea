969a5039e547740856ec5e8475e6641e1a8f47aa
keylperez <perez.williamcaleb@gmail.com>
2022-05-13 23:46:11 +0800

    helped banana but might have broken this idk

--- vue/src/components/Input/CustomDropdown.vue
+++ vue/src/components/Input/CustomDropdown.vue
@@ -1,38 +1,34 @@
 <template>
-  <div>
-  <label class="text-primary text-textInput font-semibold">
-    {{ title }}
-  </label>
-  <div class="relative">
-    <div
-      class="
-        h-[47px]
-        w-[374px]
-        gap-4
-        items-center
-        text-[#696969] text-[20px]
-        hover:cursor-pointer
-        rounded-[9px]
-        pr-[19px]
-        pl-[19px]
-        flex
-        justify-between
-      "
-
-      :class="['bg-' + background]"
-      :style="{ width: width + 'px'}"
-      :disabled="disable"
-      @click="dropdown"
-    >
-      <span>{{ selected }}</span>
-      <font-awesome-icon
-        icon="angle-down"
-        class="transition-transform"
-        :class="{ flip: isSelecting }"
-      ></font-awesome-icon>
-    </div>
-    <div class="absolute left-0 w-max h-fit z-50" v-show="isSelecting">
-      <DropdownItem
+    <div>
+        <label class="text-primary text-textInput font-semibold">
+            {{ title }}
+        </label>
+        <div class="relative">
+            <div
+                class="h-[47px] w-[374px] gap-4 items-center text-[#696969] text-[20px] hover:cursor-pointer rounded-[9px] pr-[19px] pl-[19px] flex justify-between"
+                :class="['bg-' + background]"
+                :style="{ width: width + 'px' }"
+                :disabled="disable"
+                @click="dropdown"
+            >
+                <span>{{ selected }}</span>
+                <font-awesome-icon
+                    icon="angle-down"
+                    class="transition-transform"
+                    :class="{ flip: isSelecting }"
+                ></font-awesome-icon>
+            </div>
+            <div class="absolute left-0 w-max h-fit z-50" v-show="isSelecting">
+                <DropdownItem
+                    :key="option"
+                    v-for="option in options"
+                    :text="option"
+                    v-model="selected"
+                    :first="option == options[0] ? true : false"
+                    :last="option == options[options.length - 1] ? true : false"
+                    @changeSelect="updateSelection"
+                />
+                <!-- <DropdownItem
         :key="option"
         v-for="option in options"
         :text="option"
@@ -40,57 +36,58 @@
         :first="option == options[0] ? true : false"
         :last="option == options[options.length - 1] ? true : false"
         @changeSelect="updateSelection"
-      />
+      /> -->
+            </div>
+        </div>
     </div>
-  </div>
-  </div>
 </template>
 
 <script>
 import DropdownItem from "./CustomDropdown/DropdownItem.vue";
 export default {
-  name: "CustomDropdown",
-  props: {
-    title: String,
-    default: String,
-    options: Array,
-    background: String,
-    width: Number,
-    disabled: Boolean,
-  },
-  data() {
-    return {
-      selected: String,
-      isSelecting: false,
-    };
-  },
-  computed: {
-    disable() {
-      if (this.disabled == true) {
-        this.num = "";
-      }
+    name: "CustomDropdown",
+    props: {
+        title: String,
+        default: String,
+        options: Array,
+        background: String,
+        width: Number,
+        disabled: Boolean,
+    },
+    data() {
+        return {
+            selected: String,
+            isSelecting: false,
+        };
+    },
+    computed: {
+        disable() {
+            if (this.disabled == true) {
+                this.num = "";
+            }
 
-      return this.disabled;
+            return this.disabled;
+        },
     },
-  },
-  methods: {
-    dropdown() {
-      this.isSelecting = !this.isSelecting;
+    methods: {
+        dropdown() {
+            this.isSelecting = !this.isSelecting;
+        },
+        updateSelection(newSelect) {
+            this.selected = newSelect;
+            this.$emit("changeSelect", newSelect);
+            this.dropdown();
+        },
     },
-    updateSelection(newSelect) {
-      this.selected = newSelect;
-      this.dropdown();
+    components: { DropdownItem },
+    mounted() {
+        this.selected = this.default;
     },
-  },
-  components: { DropdownItem },
-  mounted() {
-    this.selected = this.default;
-  },
 };
 </script>
 
 <style lang="postcss" scoped>
 .flip {
-  @apply rotate-180 transition-transform;
+    @apply rotate-180 transition-transform;
 }
 </style>
